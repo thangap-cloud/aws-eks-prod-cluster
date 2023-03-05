bb9fe079c99afce0ae1392601e02dcff815b68dc
Prabhu <thangaraj.prabhu@gmail.com>
2023-03-05 10:52:00 +0800

    updated the stack for support eks cluster

--- k8s-cluster-supporting-deployments.tf
+++ k8s-cluster-supporting-deployments.tf
@@ -8,5 +8,9 @@ cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_a
 #
 #  # Deploy Loki Monitoring stack
 #  # Deploy nginx ingress controller
+#  # Deploy ArgoCD
+#  # Deploy velero - for eks etcd backup
+#  # Deploy CSI Secrets provider class - for secret management
+#  # Deploy Letsencrypt - for ingress certificate management
 #}
 
